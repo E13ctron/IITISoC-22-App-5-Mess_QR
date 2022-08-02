0e28d889a0638e813bc4db8bc2f549a25daa9472
Mihir <mihirkaushik03@gmail.com>
2022-08-02 23:19:59 +0530

    commit

---
+++ app/src/main/java/com/example/messqr/sm_announce.kt
@@ -0,0 +1,11 @@
+package com.example.messqr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class sm_announce : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sm_announce)
+    }
+}
