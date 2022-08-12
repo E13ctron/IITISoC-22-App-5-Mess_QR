0ceb60b1dfa9cfe3f291f528104d61ebfed8b0a9
Mihir <mihirkaushik03@gmail.com>
2022-08-12 11:49:40 +0530

    somewhat final commit

--- app/src/main/java/com/example/messqr/MainActivity4.kt
+++ app/src/main/java/com/example/messqr/MainActivity4.kt
@@ -3,31 +3,42 @@ package com.example.messqr
 import android.content.Intent
 import android.os.Bundle
 import android.os.SystemClock
+import android.util.Log
 import android.view.PixelCopy.request
 import android.view.WindowManager
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FirebaseFirestore
 import java.util.*
 import java.util.jar.Manifest
 
 
 class MainActivity4 : AppCompatActivity() {
+    companion object {
+        private const val TAG = "MainActivity4"
+    }
 
-
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main4)
 
-
+        //init firebase auth
+        firebaseAuth=FirebaseAuth.getInstance()
+        val firebaseUser=firebaseAuth.currentUser
+        val email= firebaseUser?.email
+        val mdocref:DocumentReference=FirebaseFirestore.getInstance().document("History/${email}")
 
         //restrict the screenshot on this specific page
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.HONEYCOMB) {
             getWindow().setFlags(
                 WindowManager.LayoutParams.FLAG_SECURE,
                 WindowManager.LayoutParams.FLAG_SECURE
-            );
+            )
         }
 
 
@@ -36,11 +47,32 @@ class MainActivity4 : AppCompatActivity() {
         val hours: Int = currentTime.hours
         val scanResult: TextView = findViewById(R.id.scan_result)
         val tick: ImageView = findViewById(R.id.tick_icon)
-        if (scanid != "0") {
-            tick.setImageResource(R.drawable.ic_green_circle_200)
-            scanResult.text = "The QR Code was Scanned Successfully : "
-            scanResult.textSize = resources.getDimension(R.dimen.font_10)
-            a=1
+        if (scanid != "") {
+            var string :String=""
+            mdocref.get().addOnSuccessListener {
+
+                    string= it.getString(scanid).toString()
+
+            }
+                .addOnFailureListener {e->
+                    Log.e(TAG, e.toString())
+                }
+
+            if (string == "0")
+            {
+                tick.setImageResource(R.drawable.ic_green_circle_200)
+                scanResult.text = "The QR Code was Scanned Successfully : "
+                scanResult.textSize = resources.getDimension(R.dimen.font_10)
+                a=1
+            }
+            else if(string=="1")
+            {
+                tick.setImageResource(R.drawable.ic_qrcode_default_200)
+                scanResult.text = "Already Scanned "
+                Toast.makeText(this, "Do not scan more than one time", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
         } else {
             tick.setImageResource(R.drawable.ic_qrcode_default_200)
             scanResult.text = "Please Scan"
@@ -101,17 +133,29 @@ class MainActivity4 : AppCompatActivity() {
         //handle click scan button
         val scanButton: Button = findViewById(R.id.scan_button)
         val available: TextView = findViewById(R.id.available_text)
-        if (hours in 8..10 || hours in 12..14 || hours in 16..22) {
-                if (hours in 8..10) {
+        if (hours in 1..9 || hours in 10..14 || hours in 16..22) {
+            when (hours) {
+                in 1..9 -> {
                     available.text = "Breakfast Available"
-                } else if (hours in 12..14) {
+                }
+                in 10..14 -> {
                     available.text = "Lunch Available"
                 }
-                else if (hours in 17..22) {
+                in 16..22 -> {
                     available.text = "Dinner Available"
                 }
+            }
                 scanButton.setOnClickListener {
-                    if(scanid=="0") {
+//                    var string :String?=""
+//                    mdocref.get().addOnSuccessListener {
+//                     if(it.exists()){
+//                    string=it.getString(scanid)
+//                                       }
+//                          }
+//                .addOnFailureListener {
+//                    Log.e(TAG, "onCreate: ", )
+//                }
+                    if(scanid=="") {
                         Intent(this, QR_Scanner::class.java).also {
                             startActivity(it)
                         }
@@ -126,11 +170,12 @@ class MainActivity4 : AppCompatActivity() {
                 tick.setImageResource(R.drawable.ic_qrcode_default_200)
                 scanResult.text = "Scan the QR"
                 a=0
-                scanid="0"
+                scanid=""
                 Toast.makeText(this, "Please scan at the correct meal timing", Toast.LENGTH_SHORT)
                     .show()
             }
         }
+
     }
     override fun onBackPressed() {
         Toast.makeText(applicationContext, "Disabled Back Press for this Screen", Toast.LENGTH_SHORT).show()
